47b3b223f3fcdfc2f917bb11c71b0eada243d1e5
craigenders <cenders@ucla.edu>
2025-08-29 14:59:33 -0700

    Update Week 5 Lab. Hypothesis Testing.R

--- scripts/Week 5 Lab. Hypothesis Testing.R
+++ scripts/Week 5 Lab. Hypothesis Testing.R
@@ -1,7 +1,11 @@
+# LOAD R PACKAGES ----
+
 # load R packages
 library(ggplot2)
 library(psych)
 library(summarytools)
+
+# READ DATA ----
 
 # github url for raw data
 filepath <- 'https://raw.githubusercontent.com/craigenders/psych250a/main/data/CancerPositiveData.csv'
@@ -9,11 +13,20 @@ filepath <- 'https://raw.githubusercontent.com/craigenders/psych250a/main/data/C
 # create data frame called Cancer from github data
 Cancer <- read.csv(filepath, stringsAsFactors = T)
 
+# INSPECT DATA ----
+
 # summarize entire data frame (summarytools package)
 dfSummary(Cancer)
+
+# DESCRIPTIVE STATISTICS ----
 
 # descriptive statistics for entire data frame (psych package)
 describe(Cancer)
 
+# 95% T-TEST ----
+
 # default two-tailed test with null mean = 16 (base R)
 t.test(Cancer$Depression, mu = 16, alternative = 'two.sided')
+
+# one-tailed test in the positive direction with null mean = 16 (base R)
+t.test(Cancer$Depression, mu = 16, alternative = "greater")
