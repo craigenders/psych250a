0aa56e9833f82e02932257636ddb279d1dc8e01c
craigenders <cenders@ucla.edu>
2025-09-04 10:13:26 -0700

    Add slides

--- scripts/Week 7 Lab. Independent t-Test.R
+++ scripts/Week 7 Lab. Independent t-Test.R
@@ -7,25 +7,30 @@ library(summarytools)
 # READ DATA ----
 
 # github url for raw data
-filepath <- 'https://raw.githubusercontent.com/craigenders/psych250a/main/data/ClinicalTrialData.csv'
+filepath <- 'https://raw.githubusercontent.com/craigenders/psych250a/main/data/DiscriminationData.csv'
 
-# create data frame called ClinicalTrial from github data
-ClinicalTrial <- read.csv(filepath, stringsAsFactors = T)
+# create data frame called Discrim from github data
+Discrim <- read.csv(filepath, stringsAsFactors = T)
 
 # INSPECT DATA ----
 
 # summarize entire data frame (summarytools package)
-dfSummary(ClinicalTrial)
+dfSummary(Discrim)
+
+# DESCRIPTIVE STATISTICS ----
+
+# descriptive statistics for entire data frame (psych package)
+describe(Discrim)
 
 # DESCRIPTIVE STATISTICS BY GROUP ----
 
 # summary statistics separately by group (psych package)
-describeBy(COWeek8 ~ Condition, data = ClinicalTrial)
+describeBy(Discrimination ~ Gender, data = Discrim)
 
 # T-TEST WITH TWO-TAILED ALTERNATE HYPOTHESIS ----
 
 # independent t-test with default two-tailed alternate hypotheses (base R)
-results <- t.test(COWeek8 ~ Condition, data = ClinicalTrial)
+results <- t.test(Discrimination ~ Gender, data = Discrim)
 results
 
 # print standard error
@@ -34,4 +39,4 @@ cat('standard error of mean difference:', results$stderr)
 # STANDARIZED MEAN DIFFERENCE EFFECT SIZE ----
 
 # standardized mean difference effect size (psych package)
-cohen.d(COWeek8 ~ Condition, data = ClinicalTrial)
+cohen.d(Discrimination ~ Gender, data = Discrim)
